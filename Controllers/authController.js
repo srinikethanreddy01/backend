b76f7ace974d63284abbe060dab25609ddb1fe85
srinikethanreddy01 <153156304+srinikethanreddy01@users.noreply.github.com>
2023-12-15 17:41:07 +0530

    Update authController.js

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -10,6 +10,7 @@ const test=(req,res)=>{
 const register=async (req,res)=>{
     try{
         const{name,email,password}=req.body;
+        console.log("register")
         if(!name){
             return res.json({
                 error:'name is required'
@@ -48,7 +49,7 @@ const login=async (req,res)=>{
     try {
         
         const {email,password}=req.body;
-        console.log(email)
+        console.log("world");
         const user=await User.findOne({email});
         if(!user){
             return res.json({
